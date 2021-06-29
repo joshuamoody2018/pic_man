5841c3fc4a127e2fdd1eb0b18ba6fda38ee12f0c
Joshua Moody <joshua@jenesissoftware.com>
2021-06-29 00:45:48 -0400

    Command line arguments working. Lots of work to do.

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,62 @@ use std::fs::File;
 use std::io::{Read, Result};
 use std::path::PathBuf;
 
-pub fn get_files_by_size() -> Result<HashMap<u64, Vec<PathBuf>>> {
-    let mut size_names: HashMap<u64, Vec<PathBuf>> = HashMap::new();
+use structopt::StructOpt;
 
-    for file_entry in fs::read_dir(".")? {
+#[derive(StructOpt)]
+#[structopt(name = "Options")]
+pub struct Commands {
+    #[structopt(short = "p", long = "path", default_value = ".", parse(from_os_str))]
+    pub path: std::path::PathBuf,
+
+    #[structopt(short = "d", long = "delete_dups")]
+    pub del_dups: bool,
+
+    #[structopt(short = "r", long = "recursive")]
+    pub recursive: bool,
+
+    #[structopt(short = "s", long = "statistics")]
+    pub statistics: bool,
+}
+
+pub fn run(opts: Commands) -> Result<()> {
+    let files = get_files(&opts.path, opts.recursive)?;
+
+    if opts.del_dups {
+        del_exact_dups(get_files_by_size(&files)?)?;
+    }
+
+    if opts.statistics {
+        statistics::print_statistics(files);
+    }
+
+    Ok(())
+}
+
+pub fn get_files(path: &PathBuf, recursive: bool) -> Result<Vec<PathBuf>> {
+    let mut files = vec![];
+
+    for file_entry in fs::read_dir(&path)? {
         let file_entry = file_entry?.path();
-
-        if file_entry.is_file() {
-            size_names.entry(fs::metadata(&file_entry)?.len())
-                .or_insert(vec![])
-                .push(file_entry);
+        if recursive {
+            files.extend(get_files(&path, recursive)?);
+        } else {
+            if file_entry.is_file() {
+                files.push(file_entry)
+            }
         }
+    }
+
+    Ok(files)
+}
+
+pub fn get_files_by_size(paths: &Vec<PathBuf>) -> Result<HashMap<u64, Vec<&PathBuf>>> {
+    let mut size_names: HashMap<u64, Vec<&PathBuf>> = HashMap::new();
+
+    for file_entry in paths {
+        size_names.entry(fs::metadata(file_entry)?.len())
+            .or_insert(vec![])
+            .push(file_entry);
     }
 
     size_names.retain(|_, v| v.len() > 1);
@@ -22,7 +67,7 @@ pub fn get_files_by_size() -> Result<HashMap<u64, Vec<PathBuf>>> {
     Ok(size_names)
 }
 
-pub fn del_exact_dups(files_by_size: HashMap<u64, Vec<PathBuf>>) -> Result<()> {
+pub fn del_exact_dups(files_by_size: HashMap<u64, Vec<&PathBuf>>) -> Result<()> {
     for (_, entries) in files_by_size {
         let mut entries = entries.iter();
 
@@ -45,4 +90,23 @@ pub fn del_exact_dups(files_by_size: HashMap<u64, Vec<PathBuf>>) -> Result<()> {
     }
 
     Ok(())
+}
+
+pub mod statistics {
+    use std::path::PathBuf;
+
+    pub fn print_statistics(files: Vec<PathBuf>) {
+        print_paths_found(&files);
+    }
+
+    pub fn print_paths_found(files: &Vec<PathBuf>) {
+        print!("----STATISTICS----\n\r");
+        print!("\tPATHS FOUND:\n\r");
+
+        for file in files.iter() {
+            print!("\t\t{:?}\n\r", file);
+        }
+
+        print!("\tEND PATHS FOUND\n\r");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
 use std::process;
 use no_duplicates::*;
+use structopt::StructOpt;
 
 fn main() {
-    let files_by_size = get_files_by_size()
-        .unwrap_or_else(|e| {
-            eprintln!("Error: {}", e);
-            process::exit(1);
-        });
-
-    del_exact_dups(files_by_size)
-        .unwrap_or_else(|e| {
-            eprintln!("Error: {}", e);
-            process::exit(1);
-        });
-}
-
-
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_files_by_size() {}
+    run(Commands::from_args()).unwrap_or_else(|e| {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    });
 }
